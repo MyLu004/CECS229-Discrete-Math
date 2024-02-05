243cdf51752c7f292ec58128bc26aed5c201a251
KinokoLu <myluwork004@gmail.com>
2024-02-04 21:37:13 -0800

    update problem1,2,3

--- PA1/pa1.py
+++ PA1/pa1.py
@@ -18,16 +18,18 @@ def b_rep(n, b):
     print("it went here")
     digits = [] # stores the digits of the b-representation of n
     q = n
+    k = 0
     while q != 0:
-        # FIXME: u
-        digit = "FIXME: update 'digit' to be the remainder of q divided by b"
+        digit = q - ((q // b)*b)
         if b == 16 and digit > 9:
             hex_dict = {10: 'A', 11 : 'B', 12: 'C', 13: 'D', 14: 'E', 15 : 'F'}
-            # FIXME: Update digit = ...
-        digits.append(digit)
-        q = "FIXME: update q to the correct value."
-    return # FIXME: Return the string of digits
 
+        digits.append(digit)
+        q = q // b
+        k += 1
+    myStr = map(str, digits[::-1])
+    result = ''.join(myStr)
+    return  result
 
 """ ---------------- PROBLEM 3 ----------------"""
 
@@ -37,6 +39,7 @@ def binary_add(a, b):
     a = a.replace(' ', '')
     b = b.replace(' ', '')
 
+    myList = ""
     # padding the strings with 0's so they are the same length
     if len(a) < len(b):
         diff = len(b) - len(a)
@@ -53,17 +56,25 @@ def binary_add(a, b):
         b_i = int(b[i])
 
         # FIXME: Update result += ....
+        result = (a_i+b_i+carry) %2
         # FIXME: Update carry =
-    if carry == 1:
-        pass  # FIXME: remove
-        result += "FIXME: Update 'result' to the correct value."
-    return  # FIXME return the appropriate string
+        carry = (a_i+b_i+carry) //2
+        print('my carry: ',carry)
 
+        # myList.append(result)
+        myList += str(result)
+    if carry == 1:
+        # FIXME: remove
+        print('carry: ', carry)
+        myList += str(carry)
+        # myList.append(carry)
+
+    return myList[::-1]
+    # return  # FIXME return the appropriate string
 
 """ ---------------- PROBLEM 4 ----------------"""
-
-
 def binary_mul(a, b):
+
     # removing all whitespace from the strings
     a = a.replace(' ', '')
     b = b.replace(' ', '')
@@ -72,12 +83,19 @@ def binary_mul(a, b):
     partial_products = []
     i = 0  # index of the current bit of string 'a' beginning at 0, right-to-left
     for bit in reversed(a):
+        print("bit: ",bit)
         if bit == '1':
             partial_products.append("FIXME: Append the appropriate partial product")
         i += 1
 
     result = '0'
     while len(partial_products) > 0:
-        result = binary_add("FIXME: Input the correct arguments")
+        # result = binary_add("FIXME: Input the correct arguments")
         del partial_products[0]
     return  # FIXME: Return the appropriate result
+
+if __name__ == "__main__":
+    a = "1011"
+    b = "11"
+
+    print(binary_mul(a,b))
--- PA1/testingPA1.py
+++ PA1/testingPA1.py
@@ -1,5 +1,2 @@
-val1 = -2.9
-val2 = 2.9
-
-print("val1: ",int(val1))
-print("val2: ",int(val2))
+val1 = "12345"
+print(val1[::-1])
